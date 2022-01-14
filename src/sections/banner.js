e8d96385deac1cce0f9bdfe8dbe41aa69ac501da
nkusikevin <nkusikvnhart@gmail.com>
2022-01-14 12:10:02 +0200

    adding the banner section

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -7,7 +7,24 @@ import ShapeRight from 'assets/shape-right.png';
 
 export default function Banner() {
   return (
-    <h1>Banner</h1>
+    <section sx={styles.banner} id='home'>
+      <Container sx={styles.banner.container}>
+        <Box sx={styles.banner.contentBox}>
+          <Heading as='h1' variant="heroPrimary">
+            Top Quality Digital Products
+          </Heading>
+          <Text as='p' variant="heroSecondary">
+            We are a digital agency that specializes in building websites and mobile apps.
+          </Text>
+          <Button variant="primary">
+            Explore
+          </Button>
+          <Box sx={styles.banner.imageBox}>
+            <Image src={BannerImg} alt='banner' sx={styles.banner.image} />
+          </Box>
+        </Box>
+      </Container>
+    </section>
   );
 }
 
